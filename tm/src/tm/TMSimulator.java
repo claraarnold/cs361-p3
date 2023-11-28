7b24eaeceb8b0517722da7685820239d6dd675dd
joelathrop <jhlathrop24@gmail.com>
2023-11-28 13:09:01 -0700

    Reading cmd line args

--- tm/src/tm/TMSimulator.java
+++ tm/src/tm/TMSimulator.java
@@ -1,9 +1,30 @@
 package tm;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+
 public class TMSimulator {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        if (args.length != 1) {
+                System.err.println("Single command line argument expected");
+                System.exit(1);
+        }
+
+        try {
+            FileInputStream fstream = new FileInputStream(args[0]);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+
+            while ((strLine = br.readLine()) != null) {
+                System.out.println(strLine);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
