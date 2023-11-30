95c94d4b49f8ab12e957467c56fd98d4b69ce672
joelathrop <jhlathrop24@gmail.com>
2023-11-30 13:22:23 -0700

    found end of file

--- tm/src/tm/TMSimulator.java
+++ tm/src/tm/TMSimulator.java
@@ -15,7 +15,7 @@ public class TMSimulator {
         try {
             FileInputStream fileStream = new FileInputStream(args[0]);
             BufferedReader br = new BufferedReader(new InputStreamReader(fileStream));
-            String strLine = br.readLine();
+//            String strLine = br.readLine();
 
             // create new TM
             TM tm = new TM();
@@ -23,31 +23,38 @@ public class TMSimulator {
             int iterations = 0;     // number of times the while loop has looped
             int statesThatHaveTransitions = 0;  // increment the state to add transition to
             int numSigma = 0;   // number of characters in the alphabet
+            int numStates = 0; // number of states
             String toState; // to state to pass into addTransition
             char writeChar; // onSymb to pass into addTransition
             char direction; // direction to pass into addTransition
+            int eof = 0;
 
+            String strLine = br.readLine();
             while (strLine != null) {
+                iterations++;
+
 
                 // read and add states
-                if (iterations == 0) {
+                if (iterations == 1) {
                     for (int i = 0; i < Integer.parseInt(strLine); i++) {
                         tm.addState(Integer.toString(i));
+                        numStates++;
                     }
                     strLine = br.readLine();
                 }
 
                 // read and add sigma
-                if (iterations == 1) {
+                 else if (iterations == 2) {
                     for (int i = 0; i <= Integer.parseInt(strLine); i++) {
                         tm.addSigma((char) i);
                         numSigma++;
                     }
                     strLine = br.readLine();
+                    eof = (numStates - 1) * (numSigma);
                 }
 
                 // read and add transitions
-                if (iterations > 1) {
+                else if (iterations > 2 && iterations <= (numStates - 1) + 2) {
                     // iterate as many times as there are sigma characters
                     for (int i = 0; i < numSigma; i++) {
                         // call tm.addTransition with the following things:
@@ -66,11 +73,18 @@ public class TMSimulator {
 
                         tm.addTransition(currentState, (char) i, toState, writeChar, direction);
                         strLine = br.readLine();
+//                        iterations++;
                     }
                 }
 
+                else {
+                    System.out.println(strLine);
+                    strLine = br.readLine();
+                }
 
-                iterations++;
+
+//                iterations++;
+//                System.out.println("Iterations: " + iterations);
             }
 
         } catch (Exception e) {
